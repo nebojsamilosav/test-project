e92ae4ed36fc2b22bedc63acc55634dea274d5f2
Nebojsa Milosavljevic <shikazu@gmail.com>
2026-01-19 06:48:16 +0100

    initial structure

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "test-nmilosav-tf-state"  ### single bucket for all states
     key    = "dev/terraform.tfstate"  ### different folder/path for envs
-    region = "eu-central-1"
+    region = "eu-north-1"
   }
 }
